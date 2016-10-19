f02b9f98f1efc12b82f572fa161e3aa5917f914c
Strandberg95 <strandberg.coffe@gmail.com>
2016-10-19 14:14:27 +0200

    first commit

---
+++ api-mashup-dbserver/src/main/java/dberver/api_mashup_dbserver/App.java
@@ -0,0 +1,13 @@
+package dberver.api_mashup_dbserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
